d83e458264d8ae8a288fbd506c0c1a6fb2f7bda5
TARISO <toni.ariso@gmail.com>
2020-09-10 13:16:18 +0200

    question answer

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -7,12 +7,17 @@
 
       <hr class="my-4" />
 
-      <p>
-        List of answers
-      </p>
+      <b-list-group>
+        <b-list-group-item
+          v-for="(answer, index) in answers"
+          :key="index"
+          @click="selectAnswer(index)"
+          >{{ answer }}
+        </b-list-group-item>
+      </b-list-group>
 
       <b-button variant="primary" href="#">Submit</b-button>
-      <b-button variant="success" href="#">Next</b-button>
+      <b-button @click="next" variant="success" href="#">Next</b-button>
     </b-jumbotron>
   </div>
 </template>
@@ -21,6 +26,36 @@
 export default {
   props: {
     currentQuestion: Object,
+    next: Function,
+  },
+  data() {
+    return {
+      selectedIndex: null,
+    };
+  },
+  computed: {
+    answers() {
+      let answers = [...this.currentQuestion.incorrect_answers];
+      answers.push(this.currentQuestion.correct_answer);
+      return answers;
+    },
+  },
+  methods: {
+    selectAnswer(index) {
+      this.selectedIndex = index;
+    },
+  },
+  mounted() {
+    console.log(this.currentQuestion);
   },
 };
 </script>
+
+<style scoped>
+.list-group {
+  margin-bottom: 15px;
+}
+.btn {
+  margin: 0 5px;
+}
+</style>
